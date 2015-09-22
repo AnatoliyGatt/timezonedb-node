b864668f3bd1322637a68424ec7b1d71e72a4bfb
Anatoliy Gatt <anatoliy.gatt@aol.com>
2015-09-22 16:20:59 +0200

    Replace " with ' in definitions of all strings in index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@
  * @license MIT
  */
 
-"use strict";
+'use strict';
 
 /**
  * @public
@@ -15,4 +15,4 @@
  * @returns {Function} - Function to initialize unchangeable instance of TimeZoneDB.
  */
 
-module.exports = require("./lib/timezonedb");
+module.exports = require('./lib/timezonedb');
